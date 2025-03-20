68458e7940c973f541700eb3eeb3907ae136ca62
VIKRAMKOUNDINYA <vikramkoundinya0811@gmail.com>
2025-03-20 11:31:38 +0530

    postcss changes1

--- devmeet-view/postcss.config.js
+++ devmeet-view/postcss.config.js
@@ -1,7 +1,7 @@
-// Ensure this file is treated as an ES module
 export default {
-  plugins: [
-    (await import("tailwindcss")).default,
-    (await import("autoprefixer")).default,
-  ],
-};
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  };
+  
